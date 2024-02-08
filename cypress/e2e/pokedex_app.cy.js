e605e7bb6334aeecf14300801a3628180ee4eedb
Markku Laine <markku.laine@gmail.com>
2024-02-08 17:07:41 +0200

    feat(exercise11.9): add my solution for the exercise 11.9

---
+++ cypress/e2e/pokedex_app.cy.js
@@ -0,0 +1,14 @@
+describe('Pokedex', function () {
+  it('front page can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('Navigation works', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+
+    cy.get('.pokemon-ability-name').should('contain', 'chlorophyll').and('have.css', 'text-transform', 'capitalize')
+  })
+})
